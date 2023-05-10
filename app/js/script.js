a241c6787db7b56a3786c6a53c4d28ba9f389b19
Korniienko Yaroslav <korniienko.ya@slm.ua>
2023-05-10 13:44:53 +0300

    finished landing page

--- app/js/script.js
+++ app/js/script.js
@@ -9,11 +9,11 @@
 //    if (newEl) return newEl;
 // }
 
-import {} from "./lazy.js";
+// import {} from "./lazy.js";
 import {} from "./menu.js";
-import {} from "./spoiler.js";
+// import {} from "./spoiler.js";
 import {} from "./accordion.js";
-import {} from "./animate.js";
+// import {} from "./animate.js";
 // import {} from "./pop-up-video.js";
 // import {} from "./pop-up.js";
 // import {} from "./slider.js";
@@ -23,10 +23,6 @@ import {} from "./animate.js";
 const secondSlider = new Swiper('.secondSlider', {
     clickable: true,
     freeMode: true,
-    navigation: {
-        nextEl: 'slider__arrow-next',
-        prevEl: '.slider__arrow-prev',
-    },
     breakpoints: {
         768: {
             slidesPerView: 3,
@@ -44,6 +40,10 @@ const mainSlider = new Swiper('.mainSlider', {
     loop: true,
     thumbs: {
         swiper: secondSlider,
+    },
+    navigation: {
+        nextEl: '.slider__arrow-next',
+        prevEl: '.slider__arrow-prev',
     },
     breakpoints: {
         768: {
